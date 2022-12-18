c30cebe4748a4616b9fb5cb08ffefd208eb90add
fallen02 <000.sb13@gmail.com>
2022-12-18 23:59:18 +0530

    Search

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,49 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
 import Background from '../components/Background'
 import Card from '../components/Card'
-import HomeLoader from '../components/HomeLoader'
+import { Input, sharedVisuallyHidden } from '@nextui-org/react';
 import styles from '../styles/Card.module.css'
+import { useMemo, useState } from 'react';
 
+export async function getServerSideProps() {
+  const res = await fetch(`https://restcountries.com/v3.1/all`)
+  const data = await res.json()
+  return { props: { data} }
+}
 
+export default function Home({data}) {
+  const [filter, setFilter] = useState('')
+  const filterdCountry = useMemo(
+    () => 
+    data.filter((p) => 
+      p.name.common.toLowerCase().includes(filter.toLowerCase())
+    ),
+    [filter, data]
+  )
 
-export default function Home() {
-
-  const [data, setData] = useState([])
-  const [load, setLoad] = useState(false)
-
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => {
-      setData(data)
-    })
-    setLoad(true)
-  }, [])
-
-  if(!load){
-    return(
-      <HomeLoader/>
-    )
-  }
-  
   return(
       <div className="">
       <Head>
         <title>Countries</title>
       </Head>
       <Background/>
+     
       <div className= {styles.container}>
+      <div className={styles.title}>All Countries</div>
+      <div className={styles.containsearch}>
+      <div className={styles.search}>
+        <Input
+            clearable 
+            placeholder="Search country"
+            // eslint-disable-next-line jsx-a11y/aria-proptypes
+            aria-label
+            onChange={e => setFilter(e.target.value)}
+            />
+      </div>
+      </div>
           <div className= {styles.cards}>
-            {data.map(country => (
+            {filterdCountry.map(country => (
               <Link href={`/${country.name.common}`} key={country.name.common}>
               <Card country = {country}/>
               </Link>
